eda51f4b877162c3e1fe02f31fb8f8a7a231adf6
Saurav Sah <sah_64@yahoo.com>
2021-06-15 22:30:03 -0400

    Added form fields

--- script.js
+++ script.js
@@ -17,6 +17,11 @@ function displayBooks() {
 	}
 }
 
+function read(check) {
+	if (check === "off") return "read";
+	else "not read";
+}
+
 const theHobbit = new book("The Hobbit", "JRR Tolkien", 295, "not read");
 const dune = new book("Dune", "Spaceman", 400, "not read");
 const wallace = new book("Wallace and Gromit", "Playdough", 50, "read");
@@ -25,12 +30,18 @@ addBooktoLibrary(theHobbit);
 addBooktoLibrary(dune);
 addBooktoLibrary(wallace);
 
-displayBooks();
+//displayBooks();
 
 const button = document.querySelector("#add-button");
 const modal = document.querySelector("#myModal");
 const modal_overlay = document.querySelector("#modal-overlay");
 const exit_button = document.querySelector("#exit-btn");
+const submit = document.querySelector("#submit-button");
+
+const title_input = document.querySelector("#title");
+const author_input = document.querySelector("#author");
+const pages_input = document.querySelector("#pages");
+const read_input = document.querySelector("#read-checkbox");
 
 button.addEventListener("click", () => {
 	modal.classList.toggle("closed");
@@ -40,4 +51,28 @@ button.addEventListener("click", () => {
 exit_button.addEventListener("click", () => {
 	modal.classList.toggle("closed");
 	modal_overlay.classList.toggle("closed");
+});
+
+submit.addEventListener("click", () => {
+	modal.classList.toggle("closed");
+	modal_overlay.classList.toggle("closed");
+
+	let newTitle = title_input.value;
+	console.log(read_input.value);
+	newTitle = new book(
+		title_input.value,
+		author_input.value,
+		pages_input.value,
+		read(read_input.value)
+	);
+
+	console.log(
+		title_input.value,
+		author_input.value,
+		pages_input.value,
+		read_input.value
+	);
+
+	addBooktoLibrary(newTitle);
+	displayBooks();
 });
