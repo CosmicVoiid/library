f41f1db33824498d2dab3c4001946992a42a18e8
Saurav Sah <sah_64@yahoo.com>
2021-06-20 14:37:55 -0400

    version 1.0 of library completed

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+//constants
+const grid_container = document.querySelector("#grid-container");
+const button = document.querySelector("#add-button");
+const modal = document.querySelector("#myModal");
+const modal_overlay = document.querySelector("#modal-overlay");
+const exit_button = document.querySelector("#exit-btn");
+const form = document.querySelector("#form");
+
+const title_input = document.querySelector("#title");
+const author_input = document.querySelector("#author");
+const pages_input = document.querySelector("#pages");
+const read_input = document.querySelector("#read-checkbox");
+
 let myLibraries = [];
 
 function book(title, author, pages, read) {
@@ -22,19 +35,72 @@ function read(check) {
 	else return "not read";
 }
 
-const grid_container = document.querySelector("#grid-container");
-function createGrid() {
+let n = 0;
+
+function createBookCards() {
 	let rows = giveRows(myLibraries);
 	console.log(rows);
 	let height = rows * 200;
 	grid_container.style.cssText = `height: ${height}px;`;
 	grid_container.style.setProperty("--rows", rows);
-	for (let i = 0; i < myLibraries.length; i++) {
-		const grid = document.createElement("div");
-		grid.textContent = `Title: ${myLibraries[i].title}`;
-		grid.classList.add("book");
-		grid_container.appendChild(grid);
+
+	const grid = document.createElement("div");
+	const book_title = document.createElement("div");
+	const book_author = document.createElement("div");
+	const book_pages = document.createElement("div");
+
+	const book_read_label = document.createElement("label");
+	const book_read_input = document.createElement("input");
+	const book_slider = document.createElement("span");
+	const book_read_text = document.createElement("div");
+
+	const book_exit = document.createElement("button");
+	const book_exit_icon = document.createElement("span");
+
+	book_exit_icon.setAttribute("class", "material-icons-outlined");
+	book_exit_icon.textContent = "close";
+	book_exit_icon.style =
+		"font-size: 18px; position: relative; top: 2px; font-weight: bold;";
+	book_exit.classList.add("exit-btns");
+	book_exit.appendChild(book_exit_icon);
+
+	book_read_input.setAttribute("type", "checkbox");
+	book_read_label.classList.add("switch");
+	book_slider.classList.add("slider", "round");
+	book_read_text.textContent = "Read? ";
+	book_read_text.style.cssText =
+		"position: relative; right: 80px; top: 3px; font-size: 18px; font-weight: bold;";
+
+	if (myLibraries[n].read === "read") {
+		book_read_input.checked = true;
 	}
+
+	book_read_label.appendChild(book_read_text);
+	book_read_label.appendChild(book_read_input);
+	book_read_label.appendChild(book_slider);
+
+	book_title.textContent = `"${myLibraries[n].title}"`;
+	book_author.textContent = `by ${myLibraries[n].author}`;
+	book_pages.textContent = `${myLibraries[n].pages} pages`;
+
+	grid.classList.add("book");
+	grid.setAttribute("id", `${n}`);
+	book_title.classList.add("book-contents");
+	book_author.classList.add("book-contents");
+	book_pages.classList.add("book-contents");
+
+	grid.appendChild(book_title);
+	grid.appendChild(book_author);
+	grid.appendChild(book_pages);
+	grid.appendChild(book_read_label);
+	grid.appendChild(book_exit);
+	grid_container.appendChild(grid);
+
+	book_exit.addEventListener("click", () => {
+		grid_container.removeChild(grid);
+	});
+
+	n++;
 }
 
 function giveRows(lib) {
@@ -42,25 +108,7 @@ function giveRows(lib) {
 	else return lib.length / 3;
 }
 
-const theHobbit = new book("The Hobbit", "JRR Tolkien", 295, "not read");
-const dune = new book("Dune", "Spaceman", 400, "not read");
-const wallace = new book("Wallace and Gromit", "Clay", 50, "read");
-
-addBooktoLibrary(theHobbit);
-addBooktoLibrary(dune);
-addBooktoLibrary(wallace);
-
-//HTML Editors
-const button = document.querySelector("#add-button");
-const modal = document.querySelector("#myModal");
-const modal_overlay = document.querySelector("#modal-overlay");
-const exit_button = document.querySelector("#exit-btn");
-const form = document.querySelector("#form");
-
-const title_input = document.querySelector("#title");
-const author_input = document.querySelector("#author");
-const pages_input = document.querySelector("#pages");
-const read_input = document.querySelector("#read-checkbox");
+//DOM Editors
 
 button.addEventListener("click", () => {
 	modal.classList.toggle("closed");
@@ -77,15 +125,13 @@ form.addEventListener("submit", (e) => {
 	modal.classList.toggle("closed");
 	modal_overlay.classList.toggle("closed");
 
-	let newTitle = title_input.value;
-	newTitle = new book(
+	const newBook = new book(
 		title_input.value,
 		author_input.value,
 		pages_input.value,
 		read(read_input)
 	);
-
-	addBooktoLibrary(newTitle);
+	addBooktoLibrary(newBook);
 	displayBooks();
-	createGrid();
+	createBookCards();
 });
